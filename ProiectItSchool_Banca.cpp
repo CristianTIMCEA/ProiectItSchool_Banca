67c24dcb245c6c6a89da99f06a103eda8b1aed9c
Cristian Timcea <cristian.timcea96@gmail.com>
2022-01-20 19:11:29 +0200

    implementare meniu principal

--- ProiectItSchool_Banca.cpp
+++ ProiectItSchool_Banca.cpp
@@ -14,6 +14,33 @@ int main()
 	cout << "        4 -> Stergere conturi\n";
 	cout << "        5 -> Eliberare/Depunere\n";
 	cout << "        6 -> Detalii cont\n";
-
-
+	cout << endl;
+	int optiune;
+	cout << "   Introduceti operatia pe care o doriti: ";
+	cin >> optiune;
+	cout << endl;
+	switch (optiune)
+	{
+	case 1: 
+		cout << "       Ati ales optiunea cu numarul 1\n";
+		break;
+	case 2:
+		cout << "       Ati ales optiunea cu numarul 2\n";
+		break;
+	case 3:
+		cout << "       Ati ales optiunea cu numarul 3\n";
+		break;
+	case 4:
+		cout << "       Ati ales optiunea cu numarul 4\n";
+		break;
+	case 5:
+		cout << "       Ati ales optiunea cu numarul 5\n";
+		break; 
+	case 6:
+		cout << "       Ati ales optiunea cu numarul 6\n";
+		break;
+	default:
+		cout << "       Optiune invalida\n";
+		break;
+	}
 }
